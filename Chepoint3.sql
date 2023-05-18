ddd69f98dd812c5b9a48b5591c9f6aa4e528d3c6
Luiz Felipe <103139855+LuizSdO@users.noreply.github.com>
2023-05-18 14:55:01 -0300

    Update Chepoint3.sql

--- Chepoint3.sql
+++ Chepoint3.sql
@@ -49,9 +49,11 @@ create table Historico(
     faltas number(3),
     nota1 number(4,2),
     nota2 number(4,2),
+    media number(4,2),
     situacao varchar(25) 
+    
 );
-
+commit;
 
 
 
@@ -87,3 +89,104 @@ INSERT INTO Professor (COD_PROF, Nome_Prof, Endereco, Cidade) VALUES (5, 'Prof. 
 INSERT INTO Turma (COD_DISC, COD_TURMA, COD_PROF, ANO, Periodo) VALUES (1, 1, 1, 2023, 'Primeiro Semestre');
 INSERT INTO Turma (COD_DISC, COD_TURMA, COD_PROF, ANO, Periodo) VALUES (2, 2, 2, 2023, 'Segundo Semestre');
 INSERT INTO Turma (COD_DISC, COD_TURMA, COD_PROF, ANO, Periodo) VALUES (3, 3, 3, 2023, 'Terceiro Semestre');
+
+
+
+
+
+set serveroutput on
+set verify off
+
+
+DECLARE
+  v_ra Historico.ra%TYPE;
+  v_cod_disc Historico.cod_disc%TYPE;
+  v_cod_turma Historico.cod_turma%TYPE;
+  v_cod_prof Historico.cod_prof%TYPE;
+  v_ano Historico.ano%TYPE;
+  v_faltas Historico.faltas%TYPE;
+  v_nota1 Historico.nota1%TYPE;
+  v_nota2 Historico.nota2%TYPE;
+  v_media Historico.media%TYPE;
+  v_situacao Historico.situacao%TYPE;
+BEGIN
+  -- Solicitar os dados ao usuário
+  v_ra := &v_ra;
+  v_cod_disc := &v_cod_disc;
+  v_cod_turma := &v_cod_turma;
+  v_cod_prof := &v_cod_prof;
+  v_ano := &v_ano;
+  
+  BEGIN
+    SELECT 1
+    INTO v_ra
+    FROM Aluno
+    WHERE ra = v_ra;
+  EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+      DBMS_OUTPUT.PUT_LINE('Código inválido, digite um código de aluno válido');
+      RETURN;
+  END;
+  
+  BEGIN
+    SELECT 1
+    INTO v_cod_disc
+    FROM Disciplina
+    WHERE cod_disc = v_cod_disc;
+  EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+      DBMS_OUTPUT.PUT_LINE('Código inválido, digite um código de disciplina válido.');
+      RETURN;
+  END;
+  
+  BEGIN
+    SELECT 1
+    INTO v_cod_turma
+    FROM Turma
+    WHERE cod_turma = v_cod_turma;
+  EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+      DBMS_OUTPUT.PUT_LINE('Código inválido, digite um código para tuma válido');
+      RETURN;
+  END;
+  
+  BEGIN
+    SELECT 1
+    INTO v_cod_prof
+    FROM Professor
+    WHERE cod_prof = v_cod_prof;
+  EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+      DBMS_OUTPUT.PUT_LINE('Código inválido, digite um código de professor válido.');
+      RETURN;
+  END;
+  
+  v_faltas := &v_faltas;
+  v_nota1 := &v_nota1;
+  v_nota2 := &v_nota2;
+  
+  v_media := (v_nota1 + v_nota2) / 2;
+  
+  IF v_media >= 7 THEN
+    v_situacao := 'Aprovado';
+  ELSIF v_media >= 5 THEN
+    v_situacao := 'Recuperação';
+  ELSE
+    v_situacao := 'Reprovado';
+  END IF;
+  
+  INSERT INTO Historico (ra, cod_disc, cod_turma, cod_prof, ano, faltas, nota1, nota2, media, situacao)
+  VALUES (v_ra, v_cod_disc, v_cod_turma, v_cod_prof, v_ano, v_faltas, v_nota1, v_nota2, v_media, v_situacao);
+  
+  DBMS_OUTPUT.PUT_LINE('Dados inseridos com sucesso!');
+EXCEPTION
+  WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('Erro: ' || SQLERRM);
+END;
+
+
+
+
+
+
+select * from Historico;
